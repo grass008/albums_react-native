052829c04f7961639b1fa0e1175b346be19e133b
Fedir Klymenko <>
2018-09-28 12:38:27 +0300

    added album list component

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 /** @format */
 // Import a library to help create a component
 import React from 'React';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-   <Header headerText={'Albums111'} />
+	<View>
+   		<Header headerText={'Albums111'} />
+   		<AlbumList />
+   	</View>
 );
 
 // Render it to the device
---
+++ src/components/AlbumList.js
@@ -0,0 +1,12 @@
+import React from 'React';
+import { View, Text } from 'react-native';
+
+const AlbumList = () => {
+	return (
+			<View>
+				<Text>Album List!!!</Text>
+			</View>	
+		);
+};
+
+export default AlbumList;
