2dca53ec74fa21b8f68233a2befa5999245c5219
Fedir Klymenko <>
2018-09-28 19:26:39 +0300

    added action to the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,16 @@
 import React from 'React';
 import { Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children  }) => {
 	const { buttonStyle, textStyle } = styles
 
 	return(
 		<TouchableOpacity 
 			onPress={onPress}
 			style={buttonStyle}>
-		<Text style={textStyle}>Buy Me</Text>
+		<Text style={textStyle}>
+			{children}
+		</Text>
 		</TouchableOpacity>
 		);
 };
@@ -32,7 +34,6 @@ const styles = {
 		marginLeft: 5,
 		marginRight: 5
 	}
-}
-
+};
 
 export default Button;
