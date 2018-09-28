10af6e949d311e41d89c348194513efd1f320cc4
Fedir Klymenko <>
2018-09-28 17:09:29 +0300

    album with card section and image

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,7 +14,7 @@ const styles = {
 		borderWidth: 1,
 		borderRadius: 4,
 		borderColor: '#ddd',
-		dorderBottowWidth: 0,
+		borderBottomWidth: 0,
 		shadowColor: '#000',
 		shadowOffset: { width: 0, height: 2 },
 		shadowOpacity: 0.2,
